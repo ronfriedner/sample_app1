a770f3ab21fcb8062593960ff63d0e2df6761e19
Ron Friedner <ron.friedner@fiverr.com>
2018-06-06 13:41:56 +0300

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -52,6 +53,17 @@ class UsersController < ApplicationController
     end
 
   # Before filters
+
+  # Confirms the correct user.
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
+
+  # Confirms an admin user.
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
 
   # Confirms a logged-in user.
   def logged_in_user
